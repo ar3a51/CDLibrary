dd5a64bd362d06fb45a4f097799295cd49f43459
ar3a51 <ar3a51@yahoo.com>
2014-09-16 00:15:17 +1000

    adding saving and viewing CDs

--- CDLibrary/Controllers/CDController.cs
+++ CDLibrary/Controllers/CDController.cs
@@ -27,5 +27,38 @@ namespace CDLibrary.Controllers
 
         }
 
+        [HttpPost]
+        public ActionResult UpdateCD(CD cdDetails)
+        {
+            if(ModelState.IsValid)
+            {
+                CDRepository.updateCD(cdDetails);
+            }
+
+            return RedirectToAction("Details", "CD", new { id = cdDetails.CDid });
+
+        }
+
+        [HttpPost]
+        public ActionResult addNewCD(CD cdDetails)
+        {
+            int CDid;
+            if (ModelState.IsValid)
+            {
+                cdDetails.insertBy = "me";
+                cdDetails.updateDate = DateTime.Now;
+                cdDetails.insertDate = DateTime.Now;
+
+                CDid = CDRepository.AddNewCD(cdDetails);
+                return RedirectToAction("Details", "CD", new { id = cdDetails.CDid });
+                
+            }
+            else
+                return RedirectToAction("Add", "CD");
+
+            
+
+        }
+
     }
 }
